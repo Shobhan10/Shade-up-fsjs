352bfdab35d1c9315d94f5cf7464f1c896b855d6
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-11-10 21:51:42 +0530

    random color generator done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 function randompickerbtn() {
-  
+  const randomPicker = document.getElementById("randomPicker");
+  const hexCode = document.getElementById("randomValuePara");
+  const data = "0123456789ABCDEF";
+  let colorCode = "#";
+  for (let i = 0; i < 6; i++) {
+    colorCode += data[Math.floor(Math.random() * data.length)];
+  }
+  hexCode.textContent = colorCode;
+  randomPicker.style.backgroundColor = colorCode;
 }
 
-function hexFindbtn() {
-}
+function hexFindbtn() {}
 
-function RGBFindbtn() {
-  
-}
+function RGBFindbtn() {}
 
-function RangeSelector() {
-  
-}
+function RangeSelector() {}
 
 // hex to rgb converter
 
-String.prototype.convertToRGB = function () {
-  
-};
+String.prototype.convertToRGB = function () {};
 
-function hexConvbtn() {
-  
-}
+function hexConvbtn() {}
 
-function rgbConvbtn() {
-  
-}
+function rgbConvbtn() {}
